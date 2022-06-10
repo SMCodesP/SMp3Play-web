bf51462176badda17c503b9f946804bc0137ecf9
unknown <samuel.silva513@etec.sp.gov.br>
2022-06-10 11:19:36 -0300

    feat: create style global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import { GlobalStyle } from '@/styles/Global';
+import { themes } from '@/utils/themes';
 import { AppProps } from 'next/app';
-import '@/styles/global.css';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <ThemeProvider theme={themes[`dark`]}>
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </ThemeProvider>
+    </>
+  );
 }
--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -1,2 +1,48 @@
-import { createGlobalStyle } from "styled-components"
+import { darken } from 'polished';
+import { createGlobalStyle } from 'styled-components';
 
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  body > div {
+    line-height: 1.6;
+    font-size: 18px;
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.foreground};
+    padding: 3px;
+    width: 100%;
+    height: 100%;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  input,
+  button,
+  textarea {
+    outline: 0;
+  }
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+  ::-moz-selection {
+    background: ${({ theme }) => theme.comment};
+  }
+  ::selection {
+    background: ${({ theme }) => theme.comment};
+  }
+  ::-webkit-scrollbar-track {
+    background: ${({ theme }) => theme.comment};
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => darken(0.1, theme.cyan)};
+    border-radius: 5px;
+    transition: 0.2s background;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => darken(0.3, theme.cyan)};
+  }
+`;
